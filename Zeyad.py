4ffda9ae6d2c357e491a01f4e508cabbe19ba070
Zeyad <zeyad@pop-os.localdomain>
2022-10-21 20:58:27 +0200

    This is a new method of writing a useful message
    I hava added print_myFav_team() which prints myFavourite team
    and you know which team I will choose ^_^

--- Zeyad.py
+++ Zeyad.py
@@ -2,3 +2,6 @@ print ('My name is Zeyad')
 print ('My age is 21')
 print ('I studies in Alexandria University')
 print ('I studies Computer & Communication Engineering')
+
+def print_myFav_team():
+	print ('My Favourite team is Software ;D')
